cc48aab0acddafc8fee9d4c3d58783326dc831d2
jiayuebao <jiayueb@andrew.cmu.edu>
2019-02-10 18:06:28 -0500

    an interactive web page

---
+++ frontend/js/login.js
@@ -0,0 +1,297 @@
+(function() {
+
+  /**
+   * Initialize major event handlers
+   */
+  function init() {
+    // register event listeners
+    document.querySelector('#login-form-btn').addEventListener('click', onSessionInvalid);
+    document.querySelector('#login-btn').addEventListener('click', login);
+    document.querySelector('#register-form-btn').addEventListener('click', showRegisterForm);
+    document.querySelector('#register-btn').addEventListener('click', register);
+    validateSession();
+  }
+
+  /**
+   * Session
+   */
+  function validateSession() {
+    onSessionInvalid();
+    // The request parameters
+    var url = './login';
+    var req = JSON.stringify({});
+
+    // display loading message
+    showLoadingMessage('Validating session...');
+
+    // make AJAX call
+    ajax('GET', url, req,
+      // session is still valid
+      function(res) {
+        var result = JSON.parse(res);
+
+        if (result.status === 'OK') {
+          onSessionValid(result);
+        }
+      });
+  }
+
+  function onSessionValid(result) {
+    user_id = result.user_id;
+    user_fullname = result.name;
+
+    var loginForm = document.querySelector('#login-form');
+    var registerForm = document.querySelector('#register-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var logoutBtn = document.querySelector('#logout-link');
+
+    welcomeMsg.innerHTML = 'Welcome, ' + user_fullname;
+
+    showElement(itemNav);
+    showElement(itemList);
+    showElement(avatar);
+    showElement(welcomeMsg);
+    showElement(logoutBtn, 'inline-block');
+    hideElement(loginForm);
+    hideElement(registerForm);
+
+    initGeoLocation();
+  }
+
+  function onSessionInvalid() {
+    var loginForm = document.querySelector('#login-form');
+    var registerForm = document.querySelector('#register-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var logoutBtn = document.querySelector('#logout-link');
+
+    hideElement(itemNav);
+    hideElement(itemList);
+    hideElement(avatar);
+    hideElement(logoutBtn);
+    hideElement(welcomeMsg);
+    hideElement(registerForm);
+
+    clearLoginError();
+    showElement(loginForm);
+  }
+
+  function hideElement(element) {
+    element.style.display = 'none';
+  }
+
+  function showElement(element, style) {
+    var displayStyle = style ? style : 'block';
+    element.style.display = displayStyle;
+  }
+  
+  function showRegisterForm() {
+    var loginForm = document.querySelector('#login-form');
+    var registerForm = document.querySelector('#register-form');
+    var itemNav = document.querySelector('#item-nav');
+    var itemList = document.querySelector('#item-list');
+    var avatar = document.querySelector('#avatar');
+    var welcomeMsg = document.querySelector('#welcome-msg');
+    var logoutBtn = document.querySelector('#logout-link');
+
+    hideElement(itemNav);
+    hideElement(itemList);
+    hideElement(avatar);
+    hideElement(logoutBtn);
+    hideElement(welcomeMsg);
+    hideElement(loginForm);
+    
+    clearRegisterResult();
+    showElement(registerForm);
+  }  
+  
+
+  function initGeoLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        onPositionUpdated,
+        onLoadPositionFailed, {
+          maximumAge: 60000
+        });
+      showLoadingMessage('Retrieving your location...');
+    } else {
+      onLoadPositionFailed();
+    }
+  }
+
+  function onPositionUpdated(position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+
+    loadNearbyItems();
+  }
+
+  function onLoadPositionFailed() {
+    console.warn('navigator.geolocation is not available');
+    getLocationFromIP();
+  }
+
+  function getLocationFromIP() {
+    // get location from http://ipinfo.io/json
+    var url = 'http://ipinfo.io/json'
+    var data = null;
+
+    ajax('GET', url, data, function(res) {
+      var result = JSON.parse(res);
+      if ('loc' in result) {
+        var loc = result.loc.split(',');
+        lat = loc[0];
+        lng = loc[1];
+      } else {
+        console.warn('Getting location by IP failed.');
+      }
+      loadNearbyItems();
+    });
+  }
+
+  // -----------------------------------
+  // Login
+  // -----------------------------------
+
+  function login() {
+    var username = document.querySelector('#username').value;
+    var password = document.querySelector('#password').value;
+    password = md5(username + md5(password));
+
+    // The request parameters
+    var url = './login';
+    var req = JSON.stringify({
+      user_id : username,
+      password : password,
+    });
+
+    ajax('POST', url, req,
+      // successful callback
+      function(res) {
+        var result = JSON.parse(res);
+
+        // successfully logged in
+        if (result.status === 'OK') {
+          onSessionValid(result);
+        }
+      },
+
+      // error
+      function() {
+        showLoginError();
+      },
+      true);
+  }
+
+  function showLoginError() {
+    document.querySelector('#login-error').innerHTML = 'Invalid username or password';
+  }
+
+  function clearLoginError() {
+    document.querySelector('#login-error').innerHTML = '';
+  }
+
+  // -----------------------------------
+  // Register
+  // -----------------------------------
+
+  function register() {
+    var username = document.querySelector('#register-username').value;
+    var password = document.querySelector('#register-password').value;
+    var firstName = document.querySelector('#register-first-name').value;
+    var lastName = document.querySelector('#register-last-name').value;
+    
+    if (username === "" || password == "" || firstName === "" || lastName === "") {
+    	showRegisterResult('Please fill in all fields');
+    	return
+    }
+    
+    if (username.match(/^[a-z0-9_]+$/) === null) {
+    	showRegisterResult('Invalid username');
+    	return
+    }
+    
+    password = md5(username + md5(password));
+
+    // The request parameters
+    var url = './register';
+    var req = JSON.stringify({
+      user_id : username,
+      password : password,
+      first_name: firstName,
+      last_name: lastName,
+    });
+
+    ajax('POST', url, req,
+      // successful callback
+      function(res) {
+        var result = JSON.parse(res);
+
+        // successfully logged in
+        if (result.status === 'OK') {
+        	showRegisterResult('Succesfully registered');
+        } else {
+        	showRegisterResult('User already existed');
+        }
+      },
+
+      // error
+      function() {
+    	  showRegisterResult('Failed to register');
+      },
+      true);
+  }
+
+  function showRegisterResult(registerMessage) {
+    document.querySelector('#register-result').innerHTML = registerMessage;
+  }
+
+  function clearRegisterResult() {
+    document.querySelector('#register-result').innerHTML = '';
+  }
+
+
+  /**
+   * AJAX helper
+   *
+   * @param method - GET|POST|PUT|DELETE
+   * @param url - API end point
+   * @param data - request payload data
+   * @param successCallback - Successful callback function
+   * @param errorCallback - Error callback function
+   */
+  function ajax(method, url, data, successCallback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open(method, url, true);
+
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        successCallback(xhr.responseText);
+      } else {
+        errorCallback();
+      }
+    };
+
+    xhr.onerror = function() {
+      console.error("The request couldn't be completed.");
+      errorCallback();
+    };
+
+    if (data === null) {
+      xhr.send();
+    } else {
+      xhr.setRequestHeader("Content-Type",
+        "application/json;charset=utf-8");
+      xhr.send(data);
+    }
+  }
+
+  init();
+
+})();
